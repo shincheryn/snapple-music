5cd8604116232cf7ecb270a01752efe21f35be1b
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-08-28 20:20:14 -0700

    migration files

---
+++ migrations/versions/2023_08_28_2019-78608b3257d8_create_songs.py
@@ -0,0 +1,24 @@
+"""create songs
+
+Revision ID: 78608b3257d8
+Revises: f895f1a61548
+Create Date: 2023-08-28 20:19:22.873907
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '78608b3257d8'
+down_revision = 'f895f1a61548'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
