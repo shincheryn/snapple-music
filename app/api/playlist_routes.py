8fc794bf9cbe16235f6816dd20b49edd58ee54ce
Cheryn Shin <cherynjjaeshin@gmail.com>
2023-09-04 15:19:59 -0700

    Get details of a playlist from id -- needs testing

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -12,7 +12,7 @@ DB_FILE = os.environ.get("DB_FILE")
     b. Authenticated user required for successful response.
     c. Successful response includes only playlists created by current user.
     d. Playlist data returned has id, userId, playlist_name, createdAt, and updatedAt.
-    e. Playlist data also has User data: id, firstName, and lastName.
+    e. Playlist data also has current user data: id, firstName, and lastName.
 """
 
 #Route 1: Get Current User's Playlists
@@ -50,3 +50,45 @@ def all_playlists(playlists):
         playlists_data.append(playlist_data)
 
     return jsonify(playlists_data)
+
+# ------------------------------------------------------------------------------ #
+
+#GET DETAILS OF A PLAYLIST FROM ID
+@playlist_routes.route('/<int:playlist_id>', methods=['GET'])
+@login_required
+def get_playlist_details(playlist_id):
+    """
+    a. Gets details of playlist specified by playlist id.
+    b. Authenticated user required for successful response.
+    c. If current user is not authenticated user for playlist, access denied.
+    d. Playlist data returned has id, userId, playlist_name, createdAt, and updatedAt.
+    e. Playlist data also has current user data: id, firstName, and lastName.
+    """
+    # Find Playlist by Id
+    playlist = Playlist.query.get(playlist_id)
+
+    if not playlist:
+        return jsonify({'error': 'Playlist not found'}), 404
+
+    # Ensure that requested playlist belongs to Current User
+    if playlist.userId != current_user.id:
+        return jsonify({'error': 'Access denied'}), 403
+
+    playlist_data = {
+            'id': playlist.id,
+            'userId': playlist.userId,
+            'playlist_name': playlist.playlist_name,
+            'createdAt': playlist.createdAt,
+            'updatedAt': playlist.updatedAt,
+            'user': {
+                'id': current_user.id,
+                'firstName': current_user.firstName,
+                'lastName': current_user.lastName
+            }
+        }
+
+    return jsonify(playlist_data)
+
+# ------------------------------------------------------------------------------ #
+
+# CREATE A PLAYLIST
